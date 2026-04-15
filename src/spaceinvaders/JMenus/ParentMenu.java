73d6f57a33aefe89e720457a77447b6049c6c2fe
Lawson <iyh18@txstate.edu>
2026-04-15 18:31:54 -0500

    ParentMenu Adds Listeners
    ParentMenu now manages the listeners that are going to be used by the children to implement their features.

--- src/spaceinvaders/JMenus/ParentMenu.java
+++ src/spaceinvaders/JMenus/ParentMenu.java
@@ -2,6 +2,7 @@ package spaceinvaders.JMenus;
 
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class ParentMenu extends JMenu {
@@ -13,10 +14,21 @@ public class ParentMenu extends JMenu {
         this.buttonTitles = setButtonTitles();
         this.setText(setTitle());
 
-        // Add menu items
-        for (String title : buttonTitles) {
-            JMenuItem item = new JMenuItem(title);
-            // No action for now
+        // Add menu items with listener and paths
+        ActionListener menuListener = getMenuListener();
+        for (int i = 0; i < buttonTitles.size(); i++) {
+            JMenuItem item = new JMenuItem(buttonTitles.get(i));
+            
+            // Add listener if provided
+            if (menuListener != null) {
+                item.addActionListener(menuListener);
+            }
+            
+            // Set path for the first 3 buttons if available
+            if (i < 3 && i < buttonPaths.size()) {
+                item.setName(buttonPaths.get(i));
+            }
+            
             this.add(item);
         }
     }
@@ -32,5 +44,9 @@ public class ParentMenu extends JMenu {
 
     protected String setTitle() {
         return "parent";
+    }
+
+    protected ActionListener getMenuListener() {
+        return null;
     }
 }
