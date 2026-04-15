d6d768b3335ddf4469f1231017a9c0e163e9a93e
Lawson <iyh18@txstate.edu>
2026-04-15 13:41:55 -0500

    BulletMenu Skeleton
    This is the outline for the BulletMenu using the ParentMenu.

---
+++ src/spaceinvaders/JMenus/BulletMenu.java
@@ -0,0 +1,12 @@
+package spaceinvaders.JMenus;
+
+public class BulletMenu extends ParentMenu {
+
+    protected String menuTitle = "Bullet";
+
+    public BulletMenu() {
+        super();
+        this.setText(menuTitle);
+    }
+
+}
