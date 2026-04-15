ddde437461f92b93658fb6a2bd363df94a67b942
Lawson <iyh18@txstate.edu>
2026-04-15 13:45:04 -0500

    ShooterMenu Skeleton
    This is the outline for the ShooterMenu using the ParentMenu.

---
+++ src/spaceinvaders/JMenus/ShooterMenu.java
@@ -0,0 +1,12 @@
+package spaceinvaders.JMenus;
+
+public class ShooterMenu extends ParentMenu {
+
+    protected String menuTitle = "Shooter";
+
+    public ShooterMenu() {
+        super();
+        this.setText(menuTitle);
+    }
+
+}
