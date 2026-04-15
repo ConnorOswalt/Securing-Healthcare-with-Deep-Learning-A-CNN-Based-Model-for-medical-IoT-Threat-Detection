d7558af6b0d3203a64ffe2ad5dabc14ccbbb90d3
Lawson <iyh18@txstate.edu>
2026-04-15 13:42:47 -0500

    InvaderMenu Skeleton
    This is the outline for the InvaderMenu using the ParnetMenu.

---
+++ src/spaceinvaders/JMenus/InvaderMenu.java
@@ -0,0 +1,12 @@
+package spaceinvaders.JMenus;
+
+public class InvaderMenu extends ParentMenu {
+
+    protected String menuTitle = "Invader";
+
+    public InvaderMenu() {
+        super();
+        this.setText(menuTitle);
+    }
+
+}
