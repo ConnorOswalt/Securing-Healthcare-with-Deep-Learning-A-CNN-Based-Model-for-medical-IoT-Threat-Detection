0c5b65cd8ad1004519484fba971045bd5d23f65c
Lawson <iyh18@txstate.edu>
2026-04-15 13:43:45 -0500

    MusicMenu Skeleton
    This is the outline for the MusicMenu using the ParentMenu.

---
+++ src/spaceinvaders/JMenus/MusicMenu.java
@@ -0,0 +1,12 @@
+package spaceinvaders.JMenus;
+
+public class MusicMenu extends ParentMenu {
+
+    protected String menuTitle = "Music";
+
+    public MusicMenu() {
+        super();
+        this.setText(menuTitle);
+    }
+
+}
